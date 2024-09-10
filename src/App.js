b32de63575519aa1c210db36bf4ff2fc00546d59
Arnought <magataaron6@gmail.com>
2024-09-10 12:53:08 +0800

    TEST (1)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import Footer from './components/Footer.js';
+import Greeting from './components/Greeting.js';
 import './App.css';
+import './Custom.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World
-        </a>
-      </header>
+    <div>
+      <h1>Hello, React!</h1>
+      <p>This is a simple JSX example.</p>
+      <Greeting greeting = "Konnichiwa" name = "Aaron"/>
+      <Greeting name = "Aaron"/>
     </div>
   );
 }
